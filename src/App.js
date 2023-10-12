53ae654ca2d04ee8349ad2b8ba704c4dcecfb06f
API65015077 <65015077@kmitl.ac.th>
2023-10-12 23:04:24 +0700

    update pick-up order and fix half dot

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 // import Login from './page/login';
-import Kinglouy from './component/Popup';
+// import Kinglouy from './component/Popup';
+import OrderInfo from './page/orderInfo';
 
 function App() {
   return (
     <div>
-      <Kinglouy />
+      {/* <Kinglouy /> */}
+      <OrderInfo />
+      {/* <HugeDot /> */}
     </div>
   );
 }
