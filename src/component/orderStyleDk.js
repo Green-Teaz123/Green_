f832a4be6585d959dec319a119cd0b986a25bb87
Thongtae sirimak <65015057@kmitl.ac.th>
2023-10-14 00:02:50 +0700

    Position desktop

--- src/component/orderStyleDk.js
+++ src/component/orderStyleDk.js
@@ -4,7 +4,7 @@ import "../styles/orderStyle.css"
 export default function orderStyleDk() {
 
     return (
-        <div className="tag-order">
+        <div className="tag-order-dk">
             <h1>All order</h1>
             <p className="box-3"></p>
         </div>
