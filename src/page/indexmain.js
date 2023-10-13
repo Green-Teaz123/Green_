a30e3e401351a6d47452d290f53276d0efd1e2d8
Chanakan Sittisangumpai <65015028@kmitl.ac.th>
2023-10-13 16:10:48 +0700

    fix create order layout

--- src/page/indexmain.js
+++ src/page/indexmain.js
@@ -10,7 +10,7 @@ export default function King_louy() {
     return (
         <div>
             <div className="create-order">
-                <input type="submit" value="Create your meal +" onClick={to_the_next_package} />
+                <input type="submit" value="Create your meal +" onClick={to_the_next_package} className="only-indexmain-input"/>
             </div>
             <OrderStyle />
 
