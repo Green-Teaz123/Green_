2b4b9284e7a794ab60bd038400880cffd6600b69
ten14301 <ten077710@gmail.com>
2023-10-14 21:55:45 +0700

    hee and juu

--- src/page/orderInfo.js
+++ src/page/orderInfo.js
@@ -25,7 +25,7 @@ export default function OrderInfo() {;
     }
 
     useEffect(() => { 
-        fetch("https://localhost:5001/api/getOrder/"+props.id)
+        fetch("https://localhost:7263/api/getOrder/"+props.id)
             .then(response => {
                 if (!response.ok) {
                     throw new Error("Network response was not ok");
@@ -121,8 +121,8 @@ export default function OrderInfo() {;
                     </div>
                     
                 </div>
-            {isPopupVisible && <Popup  closePopup={togglePopup} orderName={orderName} comment={orders.comment} OrderId={props.id}/>}
-            {isPopbear && <Popbear  OrderId={props.id}/>}
+            {isPopupVisible && <Popup  closePopup={togglePopup} orderName={orderName}  OrderId={props.id}/>}
+            {isPopbear && <Popbear  Id={props.id} name={orderName} />}
             {/* <Popup  /> */}
         </div>
 
