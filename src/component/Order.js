554a2dcfe6f43b27ea05bf82e64d4d148010f7f0
ten14301 <ten077710@gmail.com>
2023-10-15 01:57:53 +0700

    isgrab

--- src/component/Order.js
+++ src/component/Order.js
@@ -6,7 +6,7 @@ export default function Order() {
     const [orders, setOrders] = useState([]);
     
     useEffect(() => {
-        fetch("https://localhost:5001/api/getOrders")
+        fetch("https://localhost:7263/api/getOrders")
             .then(response => {
                 if (!response.ok) {
                     throw new Error("Network response was not ok");
@@ -22,24 +22,27 @@ export default function Order() {
             });
     }, []);
     const navigate  = useNavigate();
-    const to_the_next_package = (id, orderName, res_name, place, comment) => {
-        console.log(orderName, res_name, place);
-        navigate("/orderInfo",{
+    const to_the_next_package = (id, orderName, res_name, isgrab, place, comment) => {
+        console.log(orderName, res_name, place, isgrab);
+        navigate("/orderInfo", {
             state: {
                 id,
-                orderName, 
+                orderName,
                 res_name,
                 place,
+                isgrab,
                 comment
-        }});
-        
-    }
+            }
+        });
+    }    
     return (
             <div className="order-box"  >
                 {orders.map(order => (               
-                    <div className="didnt-pick" key={order.id} onClick={()=> to_the_next_package(order.id ,order.orderName, order.res_name, order.place, order.comment)}  >
+                    <div className="didnt-pick" key={order.id} onClick={()=> to_the_next_package(order.id, order.orderName, order.res_name, order.isgrab, order.place, order.comment)                    }  >
+                        {console.log(order.isgrab)}
                         <div className="order-header" >
                             <h2 >Order name: {order.orderName}</h2>
+
                             <div  >
                                 <svg width="20" height="20" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     {/* Your SVG content */}
@@ -48,6 +51,7 @@ export default function Order() {
                         </div>
                         <div className="res-lo-header"> <p className="res-lo-header">Restaurant name: </p> <p className="gray-scale">{order.res_name}</p> </div>
                         <div className="res-lo-header"> <p className="res-lo-header">Pick up location: </p> <p className="gray-scale">{order.place}</p> </div>
+                        <div className="res-lo-header"> <p className="res-lo-header">STATUS: </p> <p className="gray-scale">{(order.isgrab.toString())}</p> </div>
                     </div>
                 ))}
             </div>
